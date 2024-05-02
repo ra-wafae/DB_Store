2b3b5aca041dfe5b927a28c6a14d2d6b8dc30455
ihssane-hiba <134102819+ihssane-hiba@users.noreply.github.com>
2024-05-02 17:21:27 +0100

    stores

--- DB_elodie_ihssane.sql
+++ DB_elodie_ihssane.sql
@@ -12,4 +12,14 @@ state varchar(50),
 zip_code int 
 );
 
+create table stores (
+store_id int identity(1,1) constraint PK_stores primary key,
+store_name varchar(20),
+phone varchar(13),
+email varchar(50),
+street varchar(50),
+city varchar(50),
+state varchar(50),
+zip_code int 
+);
  
