68b9a62623a2b7f93f45f2dbebd54e1952acb708
ihssane-hiba <134102819+ihssane-hiba@users.noreply.github.com>
2024-05-02 17:19:11 +0100

    customers

--- DB_elodie_ihssane.sql
+++ DB_elodie_ihssane.sql
@@ -1,3 +1,15 @@
 create database BD_Store;
 use BD_Store;
+create table customers (
+customer_id int identity(1,1) constraint PK_customers primary key,
+first_name varchar(20),
+last_name varchar(20),
+phone varchar(13),
+email varchar(50),
+street varchar(50),
+city varchar(50),
+state varchar(50),
+zip_code int 
+);
 
+ 
