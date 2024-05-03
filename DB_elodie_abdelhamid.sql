463bd1263e16105ba77ece830f5b56cb4386260b
ABDALHAMID <ABD.VOU@GMAIL.COM>
2024-05-03 18:40:07 +0100

    procedures comments
    creating comments for procedures to made

--- DB_elodie_abdelhamid.sql
+++ DB_elodie_abdelhamid.sql
@@ -1,4 +1,6 @@
-/* -_-_-_-_-_-_-_-_-_-_-_-__[ FUNCTIONS ]__-_-_-_-_-_-_-_-_-_-_-_- */
+/*-------------------------------------------------------------------------------------*/
+/*--------------------------------[ F U N C T I O N S ]--------------------------------*/
+/*-------------------------------------------------------------------------------------*/
 
 -- calculate discount for an order item
 
@@ -7,3 +9,23 @@
 -- get number of specific product sltill in stock in a specific store
 
 -- get orders from a specific customer
+
+
+
+
+
+
+/*---------------------------------------------------------------------------------------*/
+/*--------------------------------[ P R O C E D U R E S ]--------------------------------*/
+/*---------------------------------------------------------------------------------------*/
+
+-- place an order
+
+-- place order_item
+
+-- update stock quantity for a product in a store (adding products)
+
+-- update stock quantity in a store (removing products)
+
+-- add an new product
+
