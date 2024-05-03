f9dd3c41045664663d1a5f4a3bc21dba9a6fa684
ABDALHAMID <ABD.VOU@GMAIL.COM>
2024-05-03 18:28:23 +0100

    creation comment of functions that need to made

---
+++ DB_elodie_abdelhamid.sql
@@ -0,0 +1,9 @@
+/* -_-_-_-_-_-_-_-_-_-_-_-__[ FUNCTIONS ]__-_-_-_-_-_-_-_-_-_-_-_- */
+
+-- calculate discount for an order item
+
+-- calculate total price the un order item
+
+-- get number of specific product sltill in stock in a specific store
+
+-- get orders from a specific customer
